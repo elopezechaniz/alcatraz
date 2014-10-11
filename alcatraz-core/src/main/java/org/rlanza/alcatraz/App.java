3ff34a816851e5f42722a97e11c4d6e055cfe921
Rlanza <ral.sistemas@gmail.com>
2014-10-11 14:44:03 -0300

    Primera version del Prog.

--- alcatraz-core/src/main/java/org/rlanza/alcatraz/App.java
+++ alcatraz-core/src/main/java/org/rlanza/alcatraz/App.java
@@ -4,10 +4,19 @@ package org.rlanza.alcatraz;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Inmate inmate1 = new Inmate();
+		Inmate inmate2 = new Inmate();
+		Guard guard1 = new Guard();
+		Hallway hallway1 = new Hallway();
+		Cell cell1 = new Cell();
+		Door mainGate = new Door();
+		
+		
+		inmate1.setPlace(cell1);
+		inmate1.beginEscape();
+		guard1.setPlace(hallway1);
+		guard1.beginwatch();
+	}
 }
